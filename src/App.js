7d378b6104fd6427157a7f9503842a4a99d0e435
Kaung Htet Zaw <me@kaunghtet.xyz>
2018-05-31 17:51:35 +0800

    Scaffolding webgl map

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MapGL from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css';
 
+const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
+console.log(process.env);
+
 class App extends Component {
+  state = {
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: 16.77362371972506,
+      longitude: 96.16739432250611,
+      zoom: 15,
+      bearing: 1,
+      minZoom: 10,
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MapGL
+        {...this.state.viewport}
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+        onViewportChange={viewport => this.setState({ viewport })}
+      >
+      </MapGL>  
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const middlewares = [
+];
+
+let composeEnhancers = compose;
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+}
+
+const store = createStore(
+    () => { },
+    {},
+    composeEnhancers(applyMiddleware(...middlewares))
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
